e15a4412a140b725dbbf5d86c955e3efba508eba
21yanick <yanick.rupp@hftm.ch>
2023-06-13 22:42:26 +0200

    d) Funktionen für Filtern und Suchen hinzugefügt
    Dieser Commit fügt drei neue Funktionen zum Taskmanager hinzu:
    
    1. filterAufgabenNachPrioritaet(Priority prioritaet): Filtert Aufgaben nach einer bestimmten Priorität und gibt eine Liste der gefilterten Aufgaben zurück.
    
    2. filterAufgabenNachStatus(Status status): Filtert Aufgaben nach einem bestimmten Status und gibt eine Liste der gefilterten Aufgaben zurück.
    
    3. sucheAufgaben(String schluesselwort): Sucht Aufgaben, die ein bestimmtes Schlüsselwort enthalten, und gibt eine Liste der gefundenen Aufgaben zurück.
    
    Diese Funktionen verbessern die Benutzerfreundlichkeit des Taskmanagers, indem sie es den Benutzern ermöglichen, ihre Aufgaben effizienter zu verwalten und zu organisieren.

--- src/main/java/ch/hftm/App.java
+++ src/main/java/ch/hftm/App.java
@@ -1,10 +1,38 @@
 package ch.hftm;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
         Taskmanager taskManager = new Taskmanager();
         taskManager.createTask("Task 1", "Beschreibung 1", Priority.HIGH, "13.06.2023", Status.OFFEN);
         taskManager.createTask("Task 2", "Beschreibung 2", Priority.NORMAL, "13.06.2023", Status.ERLEDIGT);
+        taskManager.createTask("Task 3", "Beschreibung 3", Priority.LOW, "14.06.2023", Status.OFFEN);
+        taskManager.createTask("Task 4", "Beschreibung 4", Priority.HIGH, "15.06.2023", Status.ON_HOLD);
+
+        System.out.println("Alle Aufgaben:");
         taskManager.printTasks();
+
+        System.out.println("\nAufgaben filtern nach Status OFFEN:");
+        List<Task> gefilterteAufgabenStatus = taskManager.filterAufgabenNachStatus(Status.OFFEN);
+        for (Task task : gefilterteAufgabenStatus) {
+            System.out.println(task);
+        }
+
+        System.out.println("\nAufgaben filtern nach Priorität HIGH:");
+        List<Task> gefilterteAufgabenPrio = taskManager.filterAufgabenNachPrioritaet(Priority.HIGH);
+        for (Task task : gefilterteAufgabenPrio) {
+            System.out.println(task);
+        }
+
+        System.out.println("\nAufgabe suchen:");
+        List<Task> gefundeneAufgaben = taskManager.sucheAufgaben("Task 1");
+        if (!gefundeneAufgaben.isEmpty()) {
+            for (Task task : gefundeneAufgaben) {
+                System.out.println(task);
+            }
+        } else {
+            System.out.println("Keine Aufgaben gefunden.");
+        }
     }
 }
--- src/main/java/ch/hftm/Taskmanager.java
+++ src/main/java/ch/hftm/Taskmanager.java
@@ -20,4 +20,48 @@ public class Taskmanager {
             System.out.println(task);
         }
     }
+
+    public List<Task> filterAufgabenNachStatus(Status status) {
+        List<Task> gefilterteAufgabenStatus = new ArrayList<>();
+        for (Task aufgabe : tasks) {
+            if (aufgabe.getStatus() == status) {
+                gefilterteAufgabenStatus.add(aufgabe);
+            }
+        }
+        return gefilterteAufgabenStatus;
+    }
+
+    public List<Task> filterAufgabenNachPrioritaet(Priority prioritaet) {
+        List<Task> gefilterteAufgabenPrio = new ArrayList<>();
+        for (Task aufgabe : tasks) {
+            if (aufgabe.getPriority() == prioritaet) {
+                gefilterteAufgabenPrio.add(aufgabe);
+            }
+        }
+        return gefilterteAufgabenPrio;
+    }
+
+    public void sortiereAufgabenNachDatum() {
+        int n = tasks.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (tasks.get(j).getDatum().compareTo(tasks.get(j + 1).getDatum()) > 0) {
+                    // Tausche tasks[j+1] und tasks[j]
+                    Task temp = tasks.get(j);
+                    tasks.set(j, tasks.get(j + 1));
+                    tasks.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public List<Task> sucheAufgaben(String schluesselwort) {
+        List<Task> gefundeneAufgaben = new ArrayList<>();
+        for (Task aufgabe : tasks) {
+            if (aufgabe.getAufgabe().contains(schluesselwort) || aufgabe.getBeschreibung().contains(schluesselwort)) {
+                gefundeneAufgaben.add(aufgabe);
+            }
+        }
+        return gefundeneAufgaben;
+    }
 }
